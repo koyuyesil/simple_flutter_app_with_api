96dad0bcab0d368278fcb6d983ca23620d628ccc
Örsan AKCİYER <orsan@live.com>
2022-07-11 12:35:27 +0300

    Initial commit

--- etrade/lib/data/api/category_api.dart
+++ etrade/lib/data/api/category_api.dart
@@ -1,1 +1,11 @@
+import 'package:http/http.dart' as http;
 
+class CategoryApi {
+  static Future getCategories() async {
+    return await http.get(Uri.parse("http://localhost:3000/categories"));
+  }
+
+  static Future getCategoriesById(int id) async {
+    return await http.get(Uri.parse("http://10.0.0.2:3000/categories?id=$id"));
+  }
+}
---
+++ etrade/lib/screens/main_screen.dart
@@ -0,0 +1,99 @@
+import 'dart:convert';
+
+import 'package:etrade/data/api/category_api.dart';
+import 'package:etrade/data/api/product_api.dart';
+import 'package:flutter/material.dart';
+
+import '../models/category.dart';
+import '../models/product.dart';
+
+class MainScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return StatefulWidgetState();
+  }
+}
+
+class StatefulWidgetState extends State {
+  List<Category> categories = <Category>[];
+  List<Widget> categoryWidgets = <Widget>[];
+  List<Product> products = <Product>[];
+
+  @override
+  void initState() {
+    getCategoriesFromApi();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Alışveriş Sistemi",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blueGrey,
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Column(children: <Widget>[
+          SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: categoryWidgets,
+              ))
+        ]),
+      ),
+    );
+  }
+
+  void getCategoriesFromApi() {
+    CategoryApi.getCategories().then((response) {
+      setState(() {
+        Iterable list = jsonDecode(response.body);
+        this.categories = list.map((e) => Category.fromJson(e)).toList();
+        getCategoryWidgets();
+      });
+    });
+  }
+
+  List<Widget> getCategoryWidgets() {
+    for (int i = 0; i < categories.length; i++)
+      this.categoryWidgets.add(getCategoryWidget(categories[i]));
+    return categoryWidgets; //return şimdilik kullanılmıyor.
+  }
+
+  Widget getCategoryWidget(Category category) {
+    return OutlinedButton(
+      onPressed: () {
+        getProductsByCategoryId(category);
+      },
+      child: Text(category.categoryName,
+          style: TextStyle(color: Colors.blueGrey, letterSpacing: 0)),
+      style: OutlinedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(7),
+            side: BorderSide(color: Colors.redAccent)),
+        padding: EdgeInsets.all(20),
+      ),
+    );
+  }
+
+  void getProductsByCategoryId(Category category) {
+    ProductApi.getProductsByCategoryId(category.id).then((response) {
+      setState(() {
+        Iterable list = jsonDecode(response.body);
+        this.products =
+            list.map((product) => Product.fromJson(product)).toList();
+        getProductWidgets();
+      });
+    });
+  }
+
+  void getProductWidgets() {
+    getProductWiget();
+  }
+
+  void getProductWiget() {}
+}
